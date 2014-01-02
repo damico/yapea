675689bd05a6468a80e5252b50d120d19a414255
damico <jd.comment@gmail.com>
2014-01-02 15:09:01 -0200

    Added GPL header for java files

--- YapeaAndroid/src/org/jdamico/yapea/ImageDetailActivity.java
+++ YapeaAndroid/src/org/jdamico/yapea/ImageDetailActivity.java
@@ -1,5 +1,21 @@
 package org.jdamico.yapea;
 
+/*
+ * This file is part of YAPEA.
+ * 
+ *    YAPEA is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License (version 2) 
+ *    as published by the Free Software Foundation.
+ *
+ *    YAPEA is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with YAPEA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
--- YapeaAndroid/src/org/jdamico/yapea/commons/ActivityHelper.java
+++ YapeaAndroid/src/org/jdamico/yapea/commons/ActivityHelper.java
@@ -1,17 +1,28 @@
 package org.jdamico.yapea.commons;
 
+/*
+ * This file is part of YAPEA.
+ * 
+ *    YAPEA is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License (version 2) 
+ *    as published by the Free Software Foundation.
+ *
+ *    YAPEA is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with YAPEA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.io.File;
 import java.io.IOException;
 
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 
 public class ActivityHelper {
